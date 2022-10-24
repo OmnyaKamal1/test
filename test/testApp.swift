b7f539c07750122989de2b4f024ea8903d954b84
omnya <omnyakamal1@icloud.com>
2022-10-24 09:51:55 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Omnya Kamal  on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
